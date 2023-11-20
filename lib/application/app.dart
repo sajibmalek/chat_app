d3db7a94a6b0e0f9e285a5e8a7e7d9ef970dcd78
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2023-11-20 14:46:23 +0600

    MVP Model Added

---
+++ lib/application/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:chat_app/application/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ChatApp());
+}
