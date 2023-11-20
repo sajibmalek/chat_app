acb3e1fcaea8678838398ffd44f2d08f7c56272e
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2023-11-20 14:50:04 +0600

    ui branch init commit

--- lib/application/app.dart
+++ lib/application/app.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
+import '../presentation/ui/screens/home_screen.dart';
+
 class ChatApp extends StatelessWidget {
   const ChatApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
